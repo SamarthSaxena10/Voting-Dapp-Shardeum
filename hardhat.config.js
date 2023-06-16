056a9c0912243674ce5a0a30eb77c643ed8596c5
Samarth <samkusaxena@gmail.com>
2023-06-16 12:39:35 +0530

    new code base

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,39 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { PRIVATE_KEY } = process.env;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "sphinx",
+  networks: {
+    hardhat: {},
+    sphinx: {
+      url: "https://sphinx.shardeum.org/",
+      chainId: 8082,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
 };
